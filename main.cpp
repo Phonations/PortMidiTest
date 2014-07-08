daa30a33fae6d69dee5a0a5ded603571f3d61867
ThomasAy <thomas@phonations.com>
2014-07-08 15:54:58 +0200

    adding a switch using the status

--- main.cpp
+++ main.cpp
@@ -30,44 +30,72 @@ PmTimestamp proc(void *time_info)
 	PmEvent event;
 	Pm_Read(stream, &event, 1);
 	int status = Pm_MessageStatus(event.message);
-	int data1 = Pm_MessageData1(event.message);
-	int data2 = Pm_MessageData2(event.message);
-	int type = data1 >> 4;
-	int value = data1 & 0xf;
 
-	switch(type) {
-	case 0:
-		ff1 = value;
+	switch (status) {
+	case 0xF0:
+		qDebug() << "SysEx" << conv(event.message, 8, 16) << event.message;
 		break;
-	case 1:
-		ff2 = value;
+	case 0xF1:
+	{
+		int data1 = Pm_MessageData1(event.message);
+		int data2 = Pm_MessageData2(event.message);
+		int type = data1 >> 4;
+		int value = data1 & 0xf;
+
+		switch(type) {
+		case 0:
+			ff1 = value;
+			break;
+		case 1:
+			ff2 = value;
+			break;
+		case 2:
+			ss1 = value;
+			break;
+		case 3:
+			ss2 = value;
+		case 4:
+			mm1 = value;
+			break;
+		case 5:
+			mm2 = value;
+			break;
+		case 6:
+			hh1 = value;
+			break;
+		case 7:
+			hh2 = value & 1;
+			rate = value >> 1;
+			break;
+		}
+
+		int ff = (ff2 << 4) + ff1;
+		int ss = (ss2 << 4) + ss1;
+		int mm = (mm2 << 4) + mm1;
+		int hh = (hh2 << 4) + hh1;
+		//qDebug() << "MTC QF" << hh << mm << ss << ff;
 		break;
-	case 2:
-		ss1 = value;
+	}
+	case 0xF7:
+		qDebug() << "EO SysEx" << conv(event.message, 8, 16);
 		break;
-	case 3:
-		ss2 = value;
-	case 4:
-		mm1 = value;
+	case 0xF8:
+		//qDebug() << "timming clock";
 		break;
-	case 5:
-		mm2 = value;
-		break;
-	case 6:
-		hh1 = value;
-		break;
-	case 7:
-		hh2 = value & 1;
-		rate = value >> 1;
+
+	default:
+		qDebug() << conv(event.message, 8, 16) << conv(status, 2, 16);
+
 		break;
 	}
 
-	int ff = (ff2 << 4) + ff1;
-	int ss = (ss2 << 4) + ss1;
-	int mm = (mm2 << 4) + mm1;
-	int hh = (hh2 << 4) + hh1;
 	//qDebug() << ss << ff << event.timestamp << QString::number(status, 16)<< QString::number(data1, 2) << type << value << ff1 << ff2 << ff;
-	qDebug() << conv(event.timestamp, 8, 10) << conv(data1, 8, 2) << conv(type, 3, 2) << rate << hh << mm << ss << ff;
+	//qDebug() << event.message << conv(event.timestamp, 8, 10) << conv(data1, 8, 2) << conv(type, 3, 2) << rate << hh << mm << ss << ff << status;
+
+//	qDebug() << conv(status, 2, 16) << conv(data1, 2, 16) << conv(data2, 2, 16);
+//	qDebug() << status << data1 << data2;
+//	qDebug() << "-------------------------";
+
 
 	return 0;
 }
